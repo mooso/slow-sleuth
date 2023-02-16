420b9bbf944d1768b7428f2a89f91921f561dbf0
Mostafa Elhemali <mostafa.elhemaly@gmail.com>
2023-02-15 19:20:50 -0800

    Wire clustering to the tracing subscriber
    Add a new function - spawn_clustering_thread() - to consume timing
    information from the tracing subscriber Sleuth and automatically cluster
    them and output timing information on these clusters periodically.

--- slow-sleuth/src/lib.rs
+++ slow-sleuth/src/lib.rs
@@ -3,9 +3,14 @@
 
 use std::{
     collections::HashMap,
+    io::{self, Write},
+    sync::mpsc::{self, TrySendError},
+    thread::{spawn, JoinHandle},
     time::{Duration, Instant},
 };
 
+use clustering::{Cluster, ClusteringParameters, SleuthClassifier};
+use itertools::Itertools;
 use tracing::{Id, Subscriber};
 use tracing_subscriber::{registry::LookupSpan, Layer};
 
@@ -103,9 +108,111 @@ where
     }
 }
 
+/// Parameters for creating a separate thread to handle classification and clustering.
+pub struct ClusteringThreadParameters<W> {
+    /// The output for formatted cluster information.
+    pub output: W,
+    /// The period for outputing cluster information (e.g. if 10 seconds will output every 10 seconds).
+    pub output_period: Duration,
+    /// The parameters for the clustering algorithm.
+    pub clustering_parameters: ClusteringParameters,
+    /// Bound on the channel to the thread. If the thread falls behind and this channel fills up, timing info will be discarded.
+    pub channel_limit: usize,
+}
+
+impl<W> ClusteringThreadParameters<W> {
+    pub fn new(output: W) -> Self {
+        Self {
+            output,
+            output_period: Duration::from_secs(10),
+            clustering_parameters: Default::default(),
+            channel_limit: 1024,
+        }
+    }
+}
+
+/// Information about the thread spawned for classifying information.
+pub struct ClusteringThreadInfo<W> {
+    /// Join handle for the thread.
+    pub thread: JoinHandle<io::Result<W>>,
+    /// Channel sender fr span information to the thread.
+    /// Can be used as the `SpanInfoConsumer` for creating a `Sleuth`.
+    pub info_sender: mpsc::SyncSender<SpanInfo>,
+}
+
+/// Spawna a separate thread for clustering and classifying timings for spans.
+pub fn spawn_clustering_thread<W>(
+    mut parameters: ClusteringThreadParameters<W>,
+) -> ClusteringThreadInfo<W>
+where
+    W: Write + Send + 'static,
+{
+    let (info_sender, rx) = mpsc::sync_channel(parameters.channel_limit);
+    let thread = spawn(move || {
+        let mut clustering = SleuthClassifier::new(parameters.clustering_parameters);
+        let mut next_output = Instant::now() + parameters.output_period;
+        while let Ok(span) = rx.recv() {
+            clustering.learn_span(span);
+            if next_output <= Instant::now() {
+                output_clusters(&mut parameters.output, clustering.clusters())?;
+                next_output = Instant::now() + parameters.output_period;
+            }
+        }
+        // Output the clusters one last time
+        output_clusters(&mut parameters.output, clustering.clusters())?;
+        Ok(parameters.output)
+    });
+    ClusteringThreadInfo {
+        thread,
+        info_sender,
+    }
+}
+
+/// Output the cluster information in a formatted manner to the output.
+/// For now I'm hardcoding the format and info I'm outputting.
+fn output_clusters<W>(
+    output: &mut W,
+    clusters: HashMap<&'static str, Vec<Cluster>>,
+) -> io::Result<()>
+where
+    W: Write,
+{
+    for span_type in clusters.keys().sorted() {
+        writeln!(output, "== {span_type} ==")?;
+        for cluster in clusters.get(span_type).unwrap() {
+            writeln!(
+                output,
+                "{} - {}/{}/{}/{}",
+                cluster.name,
+                cluster.timing_micros.min(),
+                cluster.timing_micros.value_at_percentile(0.5),
+                cluster.timing_micros.value_at_quantile(0.99),
+                cluster.timing_micros.max()
+            )?;
+        }
+    }
+    Ok(())
+}
+
+impl SpanInfoConsumer for mpsc::SyncSender<SpanInfo> {
+    fn consume_span(&self, span_info: SpanInfo) {
+        match self.try_send(span_info) {
+            Ok(()) => (),
+            Err(TrySendError::Full(_)) => {
+                // Intentionally ignore errors if the channel is full.
+                // We're favoring losing some diagnostic data over blocking the main program
+            }
+            r => r.unwrap(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use std::sync::{Arc, Mutex};
+    use std::{
+        io::Cursor,
+        sync::{Arc, Mutex},
+    };
 
     use super::*;
     use tracing::{event, instrument, Level};
@@ -144,5 +251,27 @@ mod tests {
             assert_eq!(4, span.child_counts.len()); // 3 events and one span
             assert_eq!(Some(&1), span.child_counts.get("sub_work"));
         }
+    }
+
+    #[test]
+    fn clustering_thread() {
+        let output = Cursor::new(Vec::new());
+        let parameters = ClusteringThreadParameters::new(output);
+        let thread = spawn_clustering_thread(parameters);
+        {
+            let subscriber = Registry::default().with(Sleuth::new(thread.info_sender));
+            tracing::subscriber::with_default(subscriber, || {
+                for _ in 0..100 {
+                    work();
+                }
+            });
+        }
+        let output = thread.thread.join().unwrap().unwrap().into_inner();
+        let output = String::from_utf8(output).unwrap();
+        assert!(output.contains("== work =="), "Invalid output: {output}");
+        assert!(
+            output.contains("(sub_work: -0 - -0)"),
+            "Invalid output: {output}"
+        );
     }
 }
