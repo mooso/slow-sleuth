2ff3eef2432167b0712632a85819ebc434f8bb41
Mostafa Elhemali <mostafa.elhemaly@gmail.com>
2023-02-22 09:57:30 -0800

    Initial WASM API to get performance clusters
    This gets a basic performance tracking into the WASM API.
    
    It's not great - I set a global default subscriber in initializer of
    WasmPark which is iffy because now I can't have more than one
    park per page (wasn't planning to, but it's smelly) and the code
    for accessing clusters is ugly. But hey it's a good first stab.

--- polygon-park-wasm/src/lib.rs
+++ polygon-park-wasm/src/lib.rs
@@ -1,10 +1,25 @@
+use std::sync::{Arc, Mutex};
+
 use polygon_park::PolygonPark;
 use rand::thread_rng;
+use slow_sleuth::{clustering::SleuthClassifier, Sleuth, SpanInfo, SpanInfoConsumer};
+use tracing_subscriber::{prelude::*, Registry};
 use wasm_bindgen::prelude::wasm_bindgen;
+
+struct LockedClassifier {
+    classifier: Arc<Mutex<SleuthClassifier>>,
+}
+
+impl SpanInfoConsumer for LockedClassifier {
+    fn consume_span(&self, span_info: SpanInfo) {
+        self.classifier.lock().unwrap().learn_span(span_info);
+    }
+}
 
 #[wasm_bindgen]
 pub struct WasmPark {
     park: PolygonPark,
+    classifier: Arc<Mutex<SleuthClassifier>>,
 }
 
 #[wasm_bindgen]
@@ -12,7 +27,13 @@ impl WasmPark {
     pub fn new(width: f32, height: f32) -> WasmPark {
         let mut rng = thread_rng();
         let park = PolygonPark::new_random(&mut rng, width, height);
-        WasmPark { park }
+        let classifier = Arc::new(Mutex::new(SleuthClassifier::default()));
+        let sleuth = Sleuth::new(LockedClassifier {
+            classifier: classifier.clone(),
+        });
+        let subscriber = Registry::default().with(sleuth);
+        tracing::subscriber::set_global_default(subscriber).unwrap();
+        WasmPark { park, classifier }
     }
 
     pub fn tick(&mut self, millis_elapsed: f32) {
@@ -37,6 +58,40 @@ impl WasmPark {
 
     pub fn vertix_y(&self, polygon_index: usize, point_index: usize) -> f32 {
         self.park.polygons[polygon_index].geometry.vertices[point_index].y
+    }
+
+    pub fn num_tick_clusters(&self) -> usize {
+        self.classifier
+            .lock()
+            .unwrap()
+            .clusters()
+            .get("tick")
+            .map(Vec::len)
+            .unwrap_or_default()
+    }
+
+    pub fn tick_cluster_label_range(&self, cluster_index: usize, label: &str) -> String {
+        self.classifier
+            .lock()
+            .unwrap()
+            .clusters()
+            .get("tick")
+            .unwrap()[cluster_index]
+            .bins
+            .get(label)
+            .map(|b| format!("{b}"))
+            .unwrap_or_default()
+    }
+
+    pub fn tick_cluster_micros_quantile(&self, cluster_index: usize, quantile: f64) -> u64 {
+        self.classifier
+            .lock()
+            .unwrap()
+            .clusters()
+            .get("tick")
+            .unwrap()[cluster_index]
+            .timing_micros
+            .value_at_quantile(quantile)
     }
 }
 
